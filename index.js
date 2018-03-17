f8a284bee5aaaa1b0ecdbc04bef9b8c0285a98f3
Revanth <revanth0212@gmail.com>
2018-03-17 14:19:29 -0500

    Removing unnecessary files.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if (window.Worker) {
 
-    var worker = new Worker('../packages/worker.js')
+    var worker = new Worker('worker.js')
 
     function trueTimeout (timeout, callbackFunction, failureHandler) {
         if(worker) {
