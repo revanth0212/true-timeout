7b1b2b56a007ee2c1fbed889e271817901d7ea7b
Revanth <revanth0212@gmail.com>
2018-03-17 12:48:08 -0500

    Syntax fix.

--- packages/index.js
+++ packages/index.js
@@ -1,12 +1,18 @@
-function trueTimeout (timeout, callbackFunction, failureHandler) {
-    const worker = new Worker('packages/worker.js')
-    worker.onmessage(function(event) {
-        const status = event && event.data
-        if (status === 'done') {
-            callbackFunction && callbackFunction()
-        } else {
-            failureHandler && failureHandler()
+if (window.Worker) {
+
+    var worker = new Worker('../packages/worker.js')
+
+    function trueTimeout (timeout, callbackFunction, failureHandler) {
+        if(worker) {
+            worker.onmessage = function(event) {
+                var status = event && event.data
+                if (status === 'done') {
+                    callbackFunction && callbackFunction()
+                } else {
+                    failureHandler && failureHandler()
+                }
+            }
+            worker.postMessage(timeout)
         }
-    })
-    worker.postMessage(timeout)
+    }
 }
